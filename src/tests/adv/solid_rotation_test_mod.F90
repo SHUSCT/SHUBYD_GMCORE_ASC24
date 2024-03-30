e41a4bed5188455855aa2bc4d1e311d053d06cc3
PC_Qlexander <598926542@qq.com>
2024-03-30 18:28:35 +0800

    calculate trigonometric function by oneMKL

--- src/tests/adv/solid_rotation_test_mod.F90
+++ src/tests/adv/solid_rotation_test_mod.F90
@@ -68,9 +68,12 @@ contains
 
     real(r8), intent(in) :: time_in_seconds
     integer, intent(in) :: itime
+    real(8), allocatable :: coslat(:), sinlat(:), coslon(:), sinlon(:)
+    real(8) cosalpha, sinalpha
 
     integer iblk, i, j
     real(r8) lon, lat
+    integer len1, len2, len3, full_jds_no_pole, full_jde_no_pole, half_ids, half_ide, half_jds, half_jde, full_ids, full_ide
 
     do iblk = 1, size(blocks)
       associate (block   => blocks(iblk)                    , &
@@ -83,20 +86,59 @@ contains
                  mfx     => blocks(iblk)%aux%mfx_lon        , &
                  mfy     => blocks(iblk)%aux%mfy_lat        )
       dmg%d = 1; dmg_lon%d = 1; dmg_lat%d = 1
-      do j = mesh%full_jds_no_pole, mesh%full_jde_no_pole
-        lat = mesh%full_lat(j)
-        do i = mesh%half_ids, mesh%half_ide
-          lon = mesh%half_lon(i)
-          u%d(i,j,1) = u0 * (cos(lat) * cos(alpha) + sin(lat) * cos(lon) * sin(alpha))
+
+      full_jds_no_pole = mesh%full_jds_no_pole
+      full_jde_no_pole = mesh%full_jde_no_pole
+      half_ids = mesh%half_ids
+      half_ide = mesh%half_ide
+      half_jds = mesh%half_jds
+      half_jde = mesh%half_jde
+      full_ids = mesh%full_ids
+      full_ide = mesh%full_ide
+      len1 = full_jde_no_pole - full_jds_no_pole + 1
+      len2 = half_ide - half_ids + 1
+      len3 = half_jde - half_jds + 1
+      len4 = full_ide - full_ids + 1
+      
+      ! do j = mesh%full_jds_no_pole, mesh%full_jde_no_pole
+      !   lat = mesh%full_lat(j)
+      !   do i = mesh%half_ids, mesh%half_ide
+      !     lon = mesh%half_lon(i)
+      !     u%d(i,j,1) = u0 * (cos(lat) * cos(alpha) + sin(lat) * cos(lon) * sin(alpha))
+      !   end do
+      ! end do
+
+      allocate(coslat(full_jds_no_pole:full_jde_no_pole))
+      allocate(sinlat(full_jds_no_pole:full_jde_no_pole))
+      allocate(coslon(half_ids:half_ide))
+      allocate(sinlon(half_ids:half_ide))
+
+      call vdcos(len1, mesh%full_lat(full_jds_no_pole:full_jde_no_pole), coslat)
+      call vdcos(len2, mesh%half_lon(half_ids:half_ide), coslon)
+      call vdsin(len1, mesh%full_lat(full_jds_no_pole:full_jde_no_pole), sinlat)
+      call vdsin(len2, mesh%half_lon(half_ids:half_ide), sinlon)
+      cosalpha = cos(alpha)
+      sinalpha = sin(alpha)
+
+      do j = full_jds_no_pole, full_jde_no_pole
+        do i = half_ids, half_ide
+          u%d(i,j,1) = u0 * (coslat(j) * cosalpha + sinlat(j) * coslon(i) * sinalpha)
         end do
       end do
+
+      deallocate(coslat, sinlat, coslon, sinlon)
+
+      allocate(coslon(full_ids:full_ide))
+      allocate(sinlon(full_ids:full_ide))
+      
+      call vdsin(len3, mesh%half_lat(half_jds:half_jde), sinlat)
+      call vdsin(len4, mesh%full_lon(full_ids:full_ide), sinlon)
+
       call fill_halo(u)
       mfx%d = u%d
-      do j = mesh%half_jds, mesh%half_jde
-        lat = mesh%half_lat(j)
-        do i = mesh%full_ids, mesh%full_ide
-          lon = mesh%full_lon(i)
-          v%d(i,j,1) = -u0 * sin(lon) * sin(alpha)
+      do j = half_jds, half_jde
+        do i = full_ids, full_ide
+          v%d(i,j,1) = -u0 * sinlon(i) * sinalpha
         end do
       end do
       call fill_halo(v)
